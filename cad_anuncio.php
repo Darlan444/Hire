d2dded5c542e0ba938589e17ec78b859e0550834
Darlan444 <darlaninacio321@gmail.com>
2021-05-31 16:22:14 -0300

    Bloqueio formatos arquivos

--- cad_anuncio.php
+++ cad_anuncio.php
@@ -35,30 +35,52 @@ $valor          = $_POST['valor'];
 $num_comodos    = $_POST['num_comodos'];
 $visibilidade   = 1;
 
+// Faz o upload da imagem pro diretório
+$extensao_permitida = array('jpg', 'jpeg', 'JPG', 'JPEG', 'Jpg', 'Jpeg');
+
     foreach($_FILES['foto_f']['name'] as $key_image=>$val_image){
         $images_file = $_FILES['foto_f']['name'][$key_image];
         $images_file_tmp = $_FILES['foto_f']['tmp_name'][$key_image];
-        move_uploaded_file($images_file_tmp, $images_location.$images_file);
-        $image_data .= strtolower($images_file." ");
+
+        $extensao = pathinfo($images_file, PATHINFO_EXTENSION);
+
+        if (in_array($extensao, $extensao_permitida)) {
+
+            $images_file = str_replace('.', '-', basename($images_file, $extensao));
+            $new_images_file = $images_file.time().".".$extensao;
+
+        move_uploaded_file($images_file_tmp, $images_location.$new_images_file);
+        $image_data .= strtolower($new_images_file." ");
+
+        //Pega o id da seção atual
+        $id = $_SESSION['id'];
+
+        // Inserir dados no banco
+        $sql = "INSERT INTO anuncio(proprietario, cidade, bairro,
+                rua, num, cep, foto_f, tipo, whatsapp, valor, num_comodos, id_user, visibilidade) VALUES ('$proprietario','$cidade',
+                '$bairro','$rua','$num','$cep', '$image_data', '$tipo', '$whatsapp','$valor','$num_comodos' , '$id', $visibilidade)";
+
+        $new_sql = mysqli_query($mysql_db, $sql);
+
+        // Redireciona se feito o anuúncio
+        if (mysqli_affected_rows($mysql_db) != 0) {
+            header('location: index.php');
+        } else {
+            echo "Não foi possivel cadastrar";
+        }
+
+        }else{
+            echo "Você colocou arquivos não suportados pelo sistema! Arquivos permitidos: <b>.jpg</b> e <b>.jpeg</b>.";?><br>
+            <a href="anunciar.php"> Voltar </a>
+            <?php
+        }
     }
 
 
-//Pega o id da seção atual
-$id = $_SESSION['id'];
 
-// Inserir dados no banco
-$sql = "INSERT INTO anuncio(proprietario, cidade, bairro,
-        rua, num, cep, foto_f, tipo, whatsapp, valor, num_comodos, id_user, visibilidade) VALUES ('$proprietario','$cidade',
-        '$bairro','$rua','$num','$cep', '$image_data', '$tipo', '$whatsapp','$valor','$num_comodos' , '$id', $visibilidade)";
 
-$new_sql = mysqli_query($mysql_db, $sql);
 
-// Redireciona se feito o anuúncio
-if (mysqli_affected_rows($mysql_db) != 0) {
-    header('location: index.php');
-} else {
-    echo "Não foi possivel cadastrar";
-}
+
 }
 
 $mysql_db->close();
